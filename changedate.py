8deb3e45511950cc1a5d317f79f30bf59ed4821a
ricaportela <ricaportela@gmail.com>
2016-12-30 15:46:18 -0200

    Update Changedate

--- changedate.py
+++ changedate.py
@@ -1,41 +1,32 @@
 """ Calcular Data a partir de uma quantidade de minutos """
-MINUTOSENTRADA = 4000
-OPERADOR = "+"
-DATA_E, HORA_E = "31/12/2016 23:35".split(" ", 2)
-DIA_E, MES_E, ANO_E = DATA_E.split("/", 3)
-HR_E, MINU_E = HORA_E.split(":", 2)
 
-# transformar tudo em minutos
-# converter horas em minutos
+def alterar_data(data_ent, op, minutos_ent):
+    """ Calcular nova data """
+    spl_Data_ent, spl_Hora_ent = data_ent.split(" ", 2)
+    spl_Dia_ent, spl_Mes_ent, spl_Ano_ent = spl_Data_ent.split("/", 3)
+    spl_Hora_ent, spl_Minu_ent = spl_Hora_ent.split(":", 2)
 
-MIN_TOT_E = (int(HR_E) * 60) + int(MINU_E) + MINUTOSENTRADA
-print("Total de Minutos ", MIN_TOT_E)
+    # transformar tudo em minutos
+    # converter horas em minutos totais
+    Minutos_Totais = (int(spl_Hora_ent) * 60) + int(spl_Minu_ent) + minutos_ent
+    print("Total de Minutos ", Minutos_Totais)
 
-# 5415 / 60 minutos = 90.25 = .25 * 60
-TOTAL_HORAS = MIN_TOT_E / 60
+    # 5415 / 60 minutos = 90.25 => separar inteiro de casas decimais 0.25 * 60  = 15
+    # HORAS_CONV_MINUTOS = MIN_TOT_E / 60
+    
+    # 90h e 15 min
+    #I, D = divmod(HORAS_CONV_MINUTOS, 1)
+    #RESTO_MINUTOS = D * 60
+    
+    # 90h / 24h = 3.75 => separar inteiro de casas decimais = 0.75 / 24
+    #TOTAL_DIAS = QTDE_TOTAL_HORAS / 24
+    
+    #I, D = divmod(TOTAL_DIAS, 1)
+    
+    # 3d 3.75 (0.75 * 24)  = 18 h
+    #TOTAL_HORAS2 = D * 24
+    
+    #print(int(I), " Dias", int(TOTAL_HORAS2), " horas", int(TOTAL_MINUTOS), " minutos")
 
-# 90h e 15 mine
-I, D = divmod(TOTAL_HORAS, 1)
-TOTAL_MINUTOS = D * 60
-
-# 90h / 24h = 3.75 3 dias
-TOTAL_DIAS = TOTAL_HORAS / 24
-
-I, D = divmod(TOTAL_DIAS, 1)
-
-# 3d 3.75 (0.75 * 24)  = 18 h
-TOTAL_HORAS2 = D * 24
-
-print(int(I), " Dias", int(TOTAL_HORAS2), " horas", int(TOTAL_MINUTOS), " minutos")
-
-
-# 3d 18h e 15min
-# 4000 min / 60 min =  No. de horas 66.66
-# 66h e 40 min ... peguei a dízima e multipliquei por 66*60
-# Então fica assim...
-# 66 h / 24 h = No. de dias
-# Agora pego o número de dias
-# 2d 2.75 (dizima 0.75 * 24)
-# 0,75 * 24 = 18 h
-# 2D 18H 40M
-
+if __name__ == ("__main__"):
+    alterar_data("31/12/2016 23:35","+", 4000)
