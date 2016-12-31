90df71f2a3c9f213528d592263e2b2b769b62db1
ricaportela <ricaportela@gmail.com>
2016-12-31 10:26:12 -0200

    Final Version

--- changedate.py
+++ changedate.py
@@ -12,18 +12,13 @@ def change_date(date, op, value):
     # convert all to minutes
     # convert hours in minutes to adding in total minutes
     minutesTotal = (int(hourIni) * 60) + int(minuIni) + value
-    # print("Total de Minutos: ", minutesTotal)
-    # 5415 / 60 minutos = 90.25 => separate integer from decimal places 0.25 * 60  = 15
     hours_minutes_conv = minutesTotal / 60
-    # 90h e 15 min - i = integer part and d = decimal part
-    i, d = divmod(hours_minutes_conv, 1)
+    i, d = divmod(hours_minutes_conv, 1) # separate integer from decimal places 
     resto_minutos = d * 60
-    # 90h / 24h = 3.75 => separate integer from decimal places = 0.75 / 24
     total_dias = hours_minutes_conv / 24
-    i, d = divmod(total_dias, 1)
+    i, d = divmod(total_dias, 1) #  separate integer from decimal places = 0.75 / 24
     xtotal_days = i
     xtotal_minutes = d
-    # 3d 3.75 (0.75 * 24)  = 18 h
     minutesHour = xtotal_minutes * 24
     print(int(xtotal_days), " Dias", int(minutesHour), " horas", int(resto_minutos), " minutos")
 
@@ -40,5 +35,5 @@ def change_date(date, op, value):
 
 if __name__ == ("__main__"):
      change_date("01/01/2010 23:35", "+", 4000)
-#    change_date(sys.argv[1], sys.argv[2], sys.argv[3])
+
 
